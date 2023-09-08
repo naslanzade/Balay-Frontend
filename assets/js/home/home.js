fbc5768d8ec0aae3094c28f2a3650581b6e198f9
naslanzade <novrastafa@code.edu.az>
2023-09-08 17:33:41 +0400

    partially created home page

---
+++ assets/js/home/home.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  //The wowjs
+  new WOW().init();
+
+  //Sidebar
+  let filterBar = document.querySelector("#filter-for-phone .all");
+  let settingsButton = document.querySelector("#settings .all .button");
+
+  settingsButton.addEventListener("click", function () {
+    if (filterBar.classList.contains("hide-filter")) {
+      filterBar.classList.add("active-filter");
+      filterBar.classList.remove("hide-filter");
+      settingsButton.classList.add("slide-settings");
+    } else {
+      filterBar.classList.remove("active-filter");
+      filterBar.classList.add("hide-filter");
+      settingsButton.classList.remove("slide-settings");
+    }
+  });
+
+  $(window).load(function () {
+    $(".flexslider").flexslider();
+  });
+});
